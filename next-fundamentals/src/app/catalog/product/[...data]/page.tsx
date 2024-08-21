8d3b541d4ebbe8b1d391a48da4a637760f57cc49
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-08-21 09:08:12 -0300

    Conceitos do React Server Components

--- next-fundamentals/src/app/catalog/product/[...data]/page.tsx
+++ next-fundamentals/src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ProductProps {
   params: {
     // id: string // Quando testamos a pasta com id apenas
@@ -8,6 +10,16 @@ interface ProductProps {
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
 
+  console.log(params)
+  // Sem o use-client em cima, o log vem no terminal, pois o componente é Server Component
+  // Ou seja, não usa JS no client-side
+  // Se chamasse uma função com onClick de um button para fazer esse console, já precisaria do 'use client' para funcionar
+  // No caso, enviar o JS para o navegador
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
+
   // Exemplo do exibido através dos parâmetros da URL:
   // http://localhost:3000/catalog/product/3/xl/blue
   return (
@@ -15,6 +27,8 @@ export default function Product({ params }: ProductProps) {
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color} </p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
